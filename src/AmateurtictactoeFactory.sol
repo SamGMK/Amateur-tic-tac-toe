bb28d12a866e10078224cd393d8fa0125ba271c5
Samater Phi <Samaterphi77@gmail.com>
2022-06-15 05:34:47 -0700

    factory

---
+++ src/AmateurtictactoeFactory.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Amateurtictactoe} from "./Amateurtictactoe.sol";
+
+contract AmateurTictactoeFactory{
+
+uint[] public boardNumber;
+
+function newGame(address playerOne, address playerTwo) external returns(uint256 _boardNumber) {
+    _boardNumber = boardNumber.length + 1;
+    boardNumber.push(_boardNumber);
+    address newGame = new Amateurticktacktoe(address(uint160(_boardNumber)));
+
+}
+
+}
