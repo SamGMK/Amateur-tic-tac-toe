f00a63109741a382f02fbf4a0dbab0f619ed0f61
Samater Phi <Samaterphi77@gmail.com>
2022-06-15 03:19:36 -0700

    game rules

---
+++ src/Amateurticktactoe.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract TicTacToe {
+
+uint8[9] boardPositions = [1,2,3,4,5,6,7,8,9];
+
+//Hi baby Mahak
+
+function newGame() external returns(bool NewGame) {
+
+}
+
+
+function makeMove(uint8 _position) public {
+    require(checkTurn(msg.sender) == true, "Not your turn");
+    require(positionStatus(_position) == false, "Positon Not Valid");
+    
+}
+
+
+function checkTurn(address _player) internal returns(bool) {
+   if(player1Plays == player2Plays)
+}
+
+function positionStatus(uint8 _position) internal returns(bool) {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
