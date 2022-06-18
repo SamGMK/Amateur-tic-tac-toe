b886ff4e9c236a551574d9dffe89708d0ba9f529
Samater Phi <Samaterphi77@gmail.com>
2022-06-18 05:17:43 -0700

    factory update

--- src/AmateurtictactoeFactory.sol
+++ src/AmateurtictactoeFactory.sol
@@ -5,14 +5,9 @@ import {Amateurtictactoe} from "./Amateurtictactoe.sol";
 
 contract AmateurTictactoeFactory{
 
-uint[] public boardNumber;
 
-function newGame(address _playerOne, address _playerTwo) external returns(uint256 _boardNumber) {
-    _boardNumber = boardNumber.length + 1;
-    boardNumber.push(_boardNumber);
-   Amateurtictactoe _newGame = new Amateurtictactoe(_playerOne, _playerOne);(address(uint160(_boardNumber)));
-
-
-}
+function newGame(address _playerOne, address _playerTwo) external returns(address gameInstance) {
+     require(_playerOne != _playerTwo, 'Invalid Players');
+     
 
 }
