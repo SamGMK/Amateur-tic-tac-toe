3bfca232424e6c8b36feb3e64e55fa15e8d4a823
Samater Phi <Samaterphi77@gmail.com>
2022-06-18 05:40:15 -0700

    Factory update

--- src/AmateurtictactoeFactory.sol
+++ src/AmateurtictactoeFactory.sol
@@ -5,13 +5,14 @@ import {Amateurtictactoe} from "./Amateurtictactoe.sol";
 
 contract AmateurTictactoeFactory{
 
-Amateurtictactoe amateurtictactoe;
+Amateurtictactoe[] private amateurtictactoe;
 
-function newGame(address _playerOne, address _playerTwo) external returns(address gameInstance) {
+function newGame(address _playerOne, address _playerTwo) external {
      require(_playerOne != _playerTwo, "Invalid Players");
      require(_playerOne != address(0) && _playerTwo != address(0), "Invalid Players");
      
-     amateurtictactoe = new Amateurtictactoe(_playerOne, _playerTwo);
-
+     Amateurtictactoe _amateurtictactoe = new Amateurtictactoe(_playerOne, _playerTwo);
+     amateurtictactoe.push(_amateurtictactoe);
 }
+
 }
